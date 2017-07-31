efc4464098a8be113b9a939ba9ad11337726f207
SSchwarzlose <saschaschwarzlose@web.de>
2017-07-31 09:57:48 +0200

    Add files via upload
    Added projectfiles to repo

---
+++ Models/Sickday.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abcon.Models
+{
+    class Sickday : INotifyPropertyChanged
+    {
+        #region Fields
+
+        #endregion
+        #region Properties
+
+        #endregion
+        #region Constructors
+
+        #endregion
+        #region Private Methods
+
+        #endregion
+        #region Public Methods
+
+        #endregion
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
