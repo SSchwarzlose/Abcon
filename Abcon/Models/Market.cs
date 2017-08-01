ae4fc2a0330657cd7395a4e69a643d943543fbe9
Sascha Schwarzlose <saschaschwarzlose@web.de>
2017-08-01 16:32:17 +0200

    MainWindow Layout bearbeitet SQLite3 Datenbank hinzugefügt Models hinzugefügt

---
+++ Abcon/Models/Market.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Abcon.Models
+{
+    class Market : INotifyPropertyChanged
+    {
+        #region Fields
+        private ulong _id;
+        private string _name;
+        #endregion
+        #region Properties
+        public ulong ID { get { return this._id; } set { this._id = value; } }
+        public string Name { get { return this._name; } set { this._name = value; } }
+        #endregion
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
